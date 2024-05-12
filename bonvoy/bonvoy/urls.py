269e031ccde7fa567b19a45dc22cf325cd49179c
raktimde07 <raktim12346@gmail.com>
2024-05-13 00:44:58 +0530

    added contacts page via extension feature

--- bonvoy/bonvoy/urls.py
+++ bonvoy/bonvoy/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import index, contacts
 
 urlpatterns = [
+    path('contacts/', contacts, name='contacts'),
     path('', index, name='index'),
     path('admin/', admin.site.urls),
 ]
--- bonvoy/core/views.py
+++ bonvoy/core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def contacts(request):
+    return render(request, 'core/contacts.html')
