ce6795be503a6c47705fb3e63ba6750d8bd05e5a
raktimde07 <raktim12346@gmail.com>
2024-05-17 00:27:45 +0530

    Added login page

--- bonvoy/bonvoy/urls.py
+++ bonvoy/bonvoy/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import index, contacts
 
 urlpatterns = [
     path('contacts/', contacts, name='contacts'),
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    path('accounts/', include("django.contrib.auth.urls"))
 ]
